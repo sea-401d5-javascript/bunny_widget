d33f14d5c0c6bb525a058f31cec9054ad3cbc900
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-21 10:42:58 -0700

    directives and templates finished

--- stefanie-hansen/app/js/BunnyApp/directives/ThumbnailDirective.js
+++ stefanie-hansen/app/js/BunnyApp/directives/ThumbnailDirective.js
@@ -1,11 +1,11 @@
 module.exports = function(app) {
-  app.directive('descriptionDirective', function() {
+  app.directive('thumbnailDirective', function() {
     return {
       restrict: 'E',
-      templateUrl: './templates/BunnyApp/description.html',
+      templateUrl: './templates/BunnyApp/thumbnail.html',
       scope: {
-        title: '@',
         url: '@',
+        width: '@',
         description: '@'
       }
     };
