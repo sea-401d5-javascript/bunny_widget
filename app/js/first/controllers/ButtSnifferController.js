e6769f71effb54d21a3f42c8e0db81cd00e14309
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-21 11:06:11 -0700

    submitting

--- app/js/first/controllers/ButtSnifferController.js
+++ app/js/first/controllers/ButtSnifferController.js
@@ -1,13 +1,11 @@
+'use strict';
 
 module.exports = function(app) {
   app.controller('ButtSnifferController', ['$scope', function() {
     this.url = 'http://www.wagandtrain.com/wp-content/plugins/image-shadow/cache/800d8ab9cf449d827c7d4d4a24ae0cc8.jpg';
     this.height = 400;
     this.width = 400;
-    this.title = 'Butt Sniffer';
+    this.title =  'Butt Sniffer';
     this.description = 'Look at this crazy butt sniffer, how did he get there?';
-    // this.tinyImage = function() {
-    //
-    // }
   }]);
 };
