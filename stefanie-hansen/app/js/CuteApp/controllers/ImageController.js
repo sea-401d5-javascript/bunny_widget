bc74d135c9657ccb63478148a117a4e83fdac579
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-22 09:52:29 -0700

    added image urls to controller

--- stefanie-hansen/app/js/CuteApp/controllers/ImageController.js
+++ stefanie-hansen/app/js/CuteApp/controllers/ImageController.js
@@ -4,16 +4,16 @@ module.exports = function(app) {
     this.sloths = {};
     this.sloths.title = 'Sloths';
     this.sloths.description = 'The slowest and the cutest';
-    this.sloths.images = [];
+    this.sloths.images = ['http://i.imgur.com/yLQVpWg.jpg', 'http://i.imgur.com/vwPNvtT.jpg', 'http://i.imgur.com/PyMOoJn.jpg', 'http://i.imgur.com/p1zI1rv.jpg', 'http://i.imgur.com/0tA76mt.jpg', 'http://i.imgur.com/5AnQqFq.jpg'];
 
     this.redPandas = {};
     this.redPandas.title = 'Red Pandas';
     this.redPandas.description = 'Like pandas, but cuter.';
-    this.redPandas.images = [];
+    this.redPandas.images = ['http://i.imgur.com/Bui0wgS.jpg', 'http://i.imgur.com/eziiTe1.jpg', 'http://i.imgur.com/CVsYUNQ.jpg', 'http://i.imgur.com/QuKhUoR.jpg', 'http://i.imgur.com/0YXORaB.jpg', 'http://i.imgur.com/IM5yOVz.jpg', 'http://i.imgur.com/ANsdnaH.jpg'];
 
     this.kittens = {};
     this.kittens.title = 'Kittens';
     this.kittens .description = 'Nothing needs to be said.';
-    this.kittens.images = [];
+    this.kittens.images = ['http://i.imgur.com/ozEaj1Z.png', 'http://i.imgur.com/LBfAioO.jpg', 'http://i.imgur.com/a4ZxA5u.jpg', 'http://i.imgur.com/MLsKJLZ.jpg', 'http://i.imgur.com/EqxkgdR.jpg', 'http://i.imgur.com/KBHvE1c.jpg', 'http://i.imgur.com/si8LnGy.jpg'];
   }]);
 };
