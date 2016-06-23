06ded0f70ee192f80f8d2e8fa35b942efa5c97e8
Stephen Schroeder <schrode50@gmail.com>
2016-06-23 07:41:30 -0700

    tweeks

--- stephen_schroeder/app/js/CuteApp/directives/FullDirective.js
+++ stephen_schroeder/app/js/CuteApp/directives/FullDirective.js
@@ -2,10 +2,23 @@ module.exports = function(app) {
   app.directive('fullDirective', function() {
     return {
       templateUrl: './templates/CuteApp/full.html',
+      restrict: 'E',
       scope: {
-        animal: '=',
-        image: '='
-      }
+        label: '=',
+        description: '=',
+        url: '=',
+        width: '='
+      },
+      controller: function($scope){
+        console.log($scope);
+      },
+      link: function($scope, elem, attr, controller){
+        $scope.logController = function(){
+          console.log(controller);
+        };
+        $scope.zoom = controller.zoom;
+      },
+      require: '^fullDirective'
     };
   });
 };
