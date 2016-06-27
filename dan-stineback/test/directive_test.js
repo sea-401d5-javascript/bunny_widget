fe080c8d390e5af6023e43f46d45a6f93403a3c5
Dan Stineback <dstineback@gmail.com>
2016-06-27 16:05:54 -0700

    updated save after class

--- dan-stineback/test/directive_test.js
+++ dan-stineback/test/directive_test.js
@@ -7,7 +7,7 @@ require('../app/js/client');
 const bunnyTemplate = require('../app/templates/firstApp/bunnyApp.html');
 const thumbnailTemplate = require('../app/templates/firstApp/thumbnail.html');
 const imageTemplate = require('../app/templates/firstApp/image.html');
-const photoTemplate = require('../app/templates/firstApp/photo.html');
+
 
 describe('directive test', () => {
   let $httpBackend;
@@ -89,54 +89,5 @@ describe('directive test', () => {
     expect(imgWidth).toBe('400');
     expect(imgHeight).toBe('400');
   });
-
-  // it('should be a test photoApp template', () => {
-  //   $httpBackend.expectGET('./templates/firstApp/photo.html')
-  //     .respond(200, photoTemplate);
-  //   $httpBackend.expectGET('./tempalates/firstApp/bunnyApp.html')
-  //   .respond(200, bunnyTemplate);
-  //   $httpBackend.expectGET('./templates/firstApp/thumbnail.html')
-  //   .respond(200, thumbnailTemplate);
-  //   $httpBackend.expectGET('./templates/firstApp/image.html')
-  //   .respond(200, imageTemplate);
-  //
-  //   $scope.test = {
-  //     photos: [{
-  //       url: 'www.test1.com',
-  //       title: 'test photo 1'
-  //     }, {
-  //       url: 'www.test2.com',
-  //       title: 'test photo 2'
-  //     }, {
-  //       url: 'www.test3.com',
-  //       title: 'test photo 3'
-  //     }]
-  //   };
-  //
-  //   // let link = $compile('<photo-album photos="test.photos" ></photo-album');
-  //   // let directive = link($scope);
-  //   // $scope.$digest();
-  //   // $httpBackend.flush();
-  //
-  //   directive.isolateScope().mode = 'tiny';
-  //
-  //
-  //   let link = $compile('<thumbnail-display></thumbnail-display>');
-  //   let directive = link($scope);
-  //   $scope.$digest();
-  //   $httpBackend.flush();
-  //
-  //   let img = directive.find('img');
-  //   let imgUrl = img.attr('src');
-  //   let imgWidth = img.attr('width');
-  //   let imgHeight = img.attr('height');
-  //
-  //   expect(imgUrl).toBe('www.test.com');
-  //   expect(imgWidth).toBe('400');
-  //   expect(imgHeight).toBe('400');
-  //
-  //
-  //   console.log('album log', directive);
-  // });
 
 });
